0803947daf0d6612f2b51f69bb00af165b7ab6ce
Alberto Miranda <alberto.miranda@bsc.es>
2022-06-03 12:08:12 +0200

    Rename ADM_set_qos_constraints_push() to ADM_set_qos_constraints()

--- src/network/rpcs.hpp
+++ src/network/rpcs.hpp
@@ -212,17 +212,17 @@ MERCURY_GEN_PROC(ADM_get_pending_transfers_out_t,
 
 DECLARE_MARGO_RPC_HANDLER(ADM_get_pending_transfers);
 
-/// ADM_set_qos_constraints_push
+/// ADM_set_qos_constraints
 
 MERCURY_GEN_PROC(
-        ADM_set_qos_constraints_push_in_t,
+        ADM_set_qos_constraints_in_t,
         ((hg_const_string_t) (scope))((hg_const_string_t) (qos_class))(
                 (int32_t) (element_id))((hg_const_string_t) (class_value)))
 
-MERCURY_GEN_PROC(ADM_set_qos_constraints_push_out_t,
+MERCURY_GEN_PROC(ADM_set_qos_constraints_out_t,
                  ((int32_t) (ret))((int32_t) (status)))
 
-DECLARE_MARGO_RPC_HANDLER(ADM_set_qos_constraints_push);
+DECLARE_MARGO_RPC_HANDLER(ADM_set_qos_constraints);
 
 /// ADM_set_qos_constraints_pull
 
@@ -289,7 +289,6 @@ MERCURY_GEN_PROC(ADM_get_statistics_out_t,
                  ((int32_t) (ret))((hg_const_string_t) (job_statistics)))
 
 DECLARE_MARGO_RPC_HANDLER(ADM_get_statistics);
-
 
 
 //} // namespace scord::network::rpc

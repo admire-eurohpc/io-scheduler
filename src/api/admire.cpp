68aa97738a6571986679efbd5c2e88c1e9352742
Alberto Miranda <alberto.miranda@bsc.es>
2022-06-03 18:41:58 +0200

    Add ADM_update_adhoc_storage()

--- src/api/admire.cpp
+++ src/api/admire.cpp
@@ -173,7 +173,25 @@ update_adhoc_storage(const server& srv, ADM_job_handle_t job,
     (void) ctx;
     (void) adhoc_handle;
 
-    return ADM_OTHER_ERROR;
+    scord::network::rpc_client rpc_client{srv.m_protocol};
+    rpc_client.register_rpcs();
+
+    auto endp = rpc_client.lookup(srv.m_address);
+
+    LOGGER_INFO("ADM_update_adhoc_storage(...)");
+
+    ADM_update_adhoc_storage_in_t in{};
+    ADM_update_adhoc_storage_out_t out;
+
+    endp.call("ADM_update_adhoc_storage", &in, &out);
+
+    if(out.ret < 0) {
+        LOGGER_ERROR("ADM_update_adhoc_storage() = {}", out.ret);
+        return static_cast<ADM_return_t>(out.ret);
+    }
+
+    LOGGER_INFO("ADM_update_adhoc_storage() = {}", ADM_SUCCESS);
+    return ADM_SUCCESS;
 }
 
 ADM_return_t

e129ec407d6ff5ecec42dff1008d0c7ced862ae5
amanzano <ana.manzano@bsc.es>
2022-07-12 14:53:33 +0200

    ADM_set_dataset_information.c test created

---
+++ examples/c/ADM_set_dataset_information.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <admire.h>
+
+int
+main(int argc, char* argv[]) {
+
+        if(argc != 5) {
+        fprintf(stderr, "ERROR: no location provided\n");
+        fprintf(stderr, "Usage: ADM_set_dataset_information <REMOTE_IP> <RESOURCE_ID> <INFO> <JOB_ID>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int exit_status = EXIT_SUCCESS;
+    ADM_server_t server = ADM_server_create("tcp", argv[1]);
+
+    ADM_job_t job;
+    ADM_dataset_t target;
+    ADM_dataset_info_t info;
+    ADM_return_t ret = ADM_set_dataset_information(server, job,
+                            target, info);
+
+    if(ret != ADM_SUCCESS) {
+        fprintf(stdout, "ADM_set_dataset_information() remote procedure not completed "
+                        "successfully\n");
+        exit_status = EXIT_FAILURE;
+        goto cleanup;
+    }
+
+    fprintf(stdout, "ADM_set_dataset_information() remote procedure completed "
+                    "successfully\n");
+
+cleanup:
+
+    ADM_server_destroy(server);
+    exit(exit_status);
+}

89121af3722e5e22c6aae1cbb1ed5e74bc626475
amanzano <ana.manzano@bsc.es>
2022-07-12 15:49:52 +0200

    ADM_transfer_dataset.c test created

---
+++ examples/c/ADM_transfer_dataset.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <admire.h>
+
+int
+main(int argc, char* argv[]) {
+
+    if(argc != 7) {
+        fprintf(stderr, "ERROR: no location provided\n");
+        fprintf(stderr,
+                "Usage: ADM_transfer_dataset <REMOTE_IP> <SOURCE_LOCATION> <DESTINATION_LOCATION>"
+                "<QOS_CONSTRAINTS> <DISTRIBUTION> <JOB_ID>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int exit_status = EXIT_SUCCESS;
+    ADM_server_t server = ADM_server_create("tcp", argv[1]);
+
+    ADM_job_t job;
+    ADM_dataset_t** sources = NULL;
+    ADM_dataset_t** targets = NULL;
+    ADM_qos_limit_t** limits = NULL;
+    ADM_transfer_mapping_t mapping = ADM_MAPPING_ONE_TO_ONE;
+    ADM_transfer_t tx_handle;
+    ADM_return_t ret = ADM_transfer_dataset(server, job, sources, targets,
+                                            limits, mapping, &tx_handle);
+
+    if(ret != ADM_SUCCESS) {
+        fprintf(stdout,
+                "ADM_transfer_dataset() remote procedure not completed "
+                "successfully\n");
+        exit_status = EXIT_FAILURE;
+        goto cleanup;
+    }
+
+    fprintf(stdout, "ADM_transfer_dataset() remote procedure completed "
+                    "successfully\n");
+
+cleanup:
+
+    ADM_server_destroy(server);
+    exit(exit_status);
+}

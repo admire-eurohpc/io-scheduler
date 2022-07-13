528c6ab18f0c22eff2b28b249ab7781c79cacbf3
Alberto Miranda <alberto.miranda@bsc.es>
2022-07-13 06:41:19 +0200

    Add ADM_dataset_list_t and related functions

--- src/common/network/proto/rpc_types.c
+++ src/common/network/proto/rpc_types.c
@@ -24,3 +24,72 @@
 
 #include "rpc_types.h"
 
+hg_return_t
+hg_proc_ADM_dataset_list_t(hg_proc_t proc, void* data) {
+    hg_return_t ret = HG_SUCCESS;
+    ADM_dataset_list_t* list = (ADM_dataset_list_t*) data;
+    ADM_dataset_list_t tmp = NULL;
+
+    hg_size_t length = 0;
+
+    switch(hg_proc_get_op(proc)) {
+
+        case HG_ENCODE:
+            tmp = *list;
+            // write the length of the list
+            length = tmp->l_length;
+            ret = hg_proc_hg_size_t(proc, &tmp->l_length);
+
+            if(ret != HG_SUCCESS) {
+                break;
+            }
+
+            // write the list
+            for(size_t i = 0; i < length; ++i) {
+                ret = hg_proc_adm_dataset(proc, &tmp->l_datasets[i]);
+
+                if(ret != HG_SUCCESS) {
+                    break;
+                }
+            }
+            break;
+
+        case HG_DECODE: {
+
+            // find out the length of the list
+            ret = hg_proc_hg_size_t(proc, &length);
+
+            if(ret != HG_SUCCESS) {
+                break;
+            }
+
+            // loop and create list elements
+            tmp = (ADM_dataset_list_t) calloc(1, sizeof(struct adm_dataset_list));
+            tmp->l_length = length;
+            tmp->l_datasets =
+                    (adm_dataset*) calloc(length, sizeof(adm_dataset));
+
+            for(size_t i = 0; i < length; ++i) {
+                ret = hg_proc_adm_dataset(proc, &tmp->l_datasets[i]);
+
+                if(ret != HG_SUCCESS) {
+                    break;
+                }
+            }
+
+            // return the newly-created list
+            *list = tmp;
+
+            break;
+        }
+
+        case HG_FREE:
+            tmp = *list;
+            free(tmp->l_datasets);
+            free(tmp);
+            ret = HG_SUCCESS;
+            break;
+    }
+
+    return ret;
+}

8f9f15a74bc53af785a43a9c3e9448278996212e
amanzano <ana.manzano@bsc.es>
2022-08-29 09:05:39 +0200

    ADM_update_job.cpp updated with job declaration

--- examples/cxx/ADM_update_job.cpp
+++ examples/cxx/ADM_update_job.cpp
@@ -57,7 +57,9 @@ main(int argc, char* argv[]) {
             admire::adhoc_storage::access_type::read_write, 42, 100, false);
 
     admire::job_requirements reqs{inputs, outputs, std::move(p)};
-    
+
+    const auto job = admire::register_job(server, reqs);
+    /*
     try {
         [[maybe_unused]] const auto job = admire::register_job(server, reqs);
 
@@ -67,7 +69,7 @@ main(int argc, char* argv[]) {
     } catch(const std::exception& e) {
         fmt::print(stderr, "FATAL: ADM_register_job() failed: {}\n", e.what());
         exit(EXIT_FAILURE);
-    }
+    }*/
 
 
     std::vector<admire::dataset> new_inputs;

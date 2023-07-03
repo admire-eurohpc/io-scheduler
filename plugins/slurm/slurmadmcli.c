966d7d911035c39935d2ec839b912e5eb6167f70
Alberto Miranda <alberto.miranda@bsc.es>
2023-07-03 18:31:48 +0200

    plugins/slurm: Add copyright notice

--- plugins/slurm/slurmadmcli.c
+++ plugins/slurm/slurmadmcli.c
@@ -1,3 +1,29 @@
+/******************************************************************************
+ * Copyright 2022-2023, Inria, France.
+ * Copyright 2023, Barcelona Supercomputing Center (BSC), Spain.
+ * All rights reserved.
+ *
+ * This software was partially supported by the EuroHPC-funded project ADMIRE
+ *   (Project ID: 956748, https://www.admire-eurohpc.eu).
+ *
+ * This file is part of scord.
+ *
+ * scord is free software: you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * scord is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the Lesser GNU General Public License
+ * along with scord.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *****************************************************************************/
+
 #include <errno.h>
 #include <stdint.h>				/* SIZE_MAX, uint32_t, etc. */
 #include <stdlib.h>				/* strtoul, getenv, reallocarray */

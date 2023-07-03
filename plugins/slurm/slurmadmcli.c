6ca42581d581f26a18e3936c845620c4ddaf8121
Alberto Miranda <alberto.miranda@bsc.es>
2023-07-03 19:13:26 +0200

    plugins/slurm: Unify formatting

--- plugins/slurm/slurmadmcli.c
+++ plugins/slurm/slurmadmcli.c
@@ -25,9 +25,9 @@
  *****************************************************************************/
 
 #include <errno.h>
-#include <stdint.h>				/* SIZE_MAX, uint32_t, etc. */
-#include <stdlib.h>				/* strtoul, getenv, reallocarray */
-#include <string.h>				/* strchr, strncmp, strncpy */
+#include <stdint.h> /* SIZE_MAX, uint32_t, etc. */
+#include <stdlib.h> /* strtoul, getenv, reallocarray */
+#include <string.h> /* strchr, strncmp, strncpy */
 #include <slurm/slurm.h>
 #include <slurm/spank.h>
 
@@ -44,15 +44,17 @@
  *   characters, including NULL byte
  **/
 
-#define ADHOCID_LEN  64
-#define INT32_STR_LEN 16  /* 16 chars are enough to fit an int32 in decimal */
+#define ADHOCID_LEN   64
+#define INT32_STR_LEN 16 /* 16 chars are enough to fit an int32 in decimal */
 
 #define TAG_NNODES     1
 #define TAG_WALLTIME   2
 #define TAG_MODE       3
 #define TAG_CONTEXT_ID 4
 
+// clang-format off
 SPANK_PLUGIN (admire-cli, 1)
+// clang-format on
 
 static int scord_flag = 0;
 
@@ -60,190 +62,174 @@ static int scord_flag = 0;
 static long adhoc_nnodes = 0;
 static long adhoc_walltime = 0;
 static ADM_adhoc_mode_t adhoc_mode = 0;
-static char adhoc_id[ADHOCID_LEN] = { 0 };
-
-static int process_opts(int val, const char *optarg, int remote);
-
-struct spank_option spank_opts [] = {
-	{
-		"adm-adhoc-nodes",
-		"[nnodes]",
-		"Dedicate [nnodes] to the ad-hoc storage",
-		1,                           /* option takes an argument */
-		TAG_NNODES,                  /* option tag */
-		(spank_opt_cb_f)process_opts /* callback  */
-	},
-	{
-		"adm-adhoc-walltime",
-		"[walltime]",
-		"Reserve the ad-hoc storage for [walltime] seconds",
-		1,
-		TAG_WALLTIME,
-		(spank_opt_cb_f)process_opts
-	},
-	{
-		"adm-adhoc-context",
-		"[context]",
-		"Mode of operation for the ad-hoc storage: in_job:shared|dedicated | separate:new|existing",
-		1,
-		TAG_MODE,
-		(spank_opt_cb_f)process_opts
-	},
-	{
-		"adm-adhoc-context-id",
-		"[context_id]",
-		"Context ID of the ad-hoc storage",
-		1,
-		TAG_CONTEXT_ID,
-		(spank_opt_cb_f)process_opts
-	},
-	SPANK_OPTIONS_TABLE_END
-};
+static char adhoc_id[ADHOCID_LEN] = {0};
 
 static int
-process_opts(int tag, const char *optarg, int remote)
-{
-	(void)remote;
+process_opts(int val, const char* optarg, int remote);
 
-	/* srun & sbatch/salloc */
-	spank_context_t sctx = spank_context();
-	if (sctx != S_CTX_LOCAL && sctx != S_CTX_ALLOCATOR)
-		return 0;
+struct spank_option spank_opts[] = {
+        {
+                "adm-adhoc-nodes", "[nnodes]",
+                "Dedicate [nnodes] to the ad-hoc storage",
+                1,                            /* option takes an argument */
+                TAG_NNODES,                   /* option tag */
+                (spank_opt_cb_f) process_opts /* callback  */
+        },
+        {"adm-adhoc-walltime", "[walltime]",
+         "Reserve the ad-hoc storage for [walltime] seconds", 1, TAG_WALLTIME,
+         (spank_opt_cb_f) process_opts},
+        {"adm-adhoc-context", "[context]",
+         "Mode of operation for the ad-hoc storage: in_job:shared|dedicated | separate:new|existing",
+         1, TAG_MODE, (spank_opt_cb_f) process_opts},
+        {"adm-adhoc-context-id", "[context_id]",
+         "Context ID of the ad-hoc storage", 1, TAG_CONTEXT_ID,
+         (spank_opt_cb_f) process_opts},
+        SPANK_OPTIONS_TABLE_END};
 
-	/* if we're here some scord options were passed to the Slurm CLI */
-	scord_flag = 1;
+static int
+process_opts(int tag, const char* optarg, int remote) {
+    (void) remote;
 
-	if (tag == TAG_NNODES || tag == TAG_WALLTIME) {
-		long tmp;
-		char *endptr;
-		errno = 0;
+    /* srun & sbatch/salloc */
+    spank_context_t sctx = spank_context();
+    if(sctx != S_CTX_LOCAL && sctx != S_CTX_ALLOCATOR)
+        return 0;
 
-		tmp = strtol(optarg, &endptr, 0);
-		if (errno != 0 || endptr == optarg || *endptr != '\0' || tmp <= 0) {
-			return -1;
-		}
+    /* if we're here some scord options were passed to the Slurm CLI */
+    scord_flag = 1;
 
-		if (tag == TAG_NNODES) {
-			adhoc_nnodes = tmp;
-		}
-		if (tag == TAG_WALLTIME) {
-			adhoc_walltime = tmp;
-		}
+    if(tag == TAG_NNODES || tag == TAG_WALLTIME) {
+        long tmp;
+        char* endptr;
+        errno = 0;
 
-		return 0;
-	}
+        tmp = strtol(optarg, &endptr, 0);
+        if(errno != 0 || endptr == optarg || *endptr != '\0' || tmp <= 0) {
+            return -1;
+        }
 
-	if (tag == TAG_MODE) {
-		char *col = strchr(optarg, ':');
-		int parsed = 0;
+        if(tag == TAG_NNODES) {
+            adhoc_nnodes = tmp;
+        }
+        if(tag == TAG_WALLTIME) {
+            adhoc_walltime = tmp;
+        }
 
-		if (col) {
-			if (!strncmp(optarg, "in_job", 6)) {
-				if (!strncmp(col + 1, "shared", 6)) {
-					adhoc_mode = ADM_ADHOC_MODE_IN_JOB_SHARED;
-					parsed = 1;
-				}
-				if (!strncmp(col + 1, "dedicated", 9)) {
-					adhoc_mode = ADM_ADHOC_MODE_IN_JOB_DEDICATED;
-					parsed = 1;
-				}
-			} else if (!strncmp(optarg, "separate", 8)) {
-				if (!strncmp(col + 1, "new", 3)) {
-					adhoc_mode = ADM_ADHOC_MODE_SEPARATE_NEW;
-					parsed = 1;
-				}
-				if (!strncmp(col + 1, "existing", 8)) {
-					adhoc_mode = ADM_ADHOC_MODE_SEPARATE_EXISTING;
-					parsed = 1;
-				}
-			}
-		}
+        return 0;
+    }
 
-		if (!parsed) {
-			return -1;
-		}
-	}
+    if(tag == TAG_MODE) {
+        char* col = strchr(optarg, ':');
+        int parsed = 0;
 
-	if (tag == TAG_CONTEXT_ID) {
-		strncpy(adhoc_id, optarg, ADHOCID_LEN - 1);
-		adhoc_id[ADHOCID_LEN - 1] = '\0';
-	}
+        if(col) {
+            if(!strncmp(optarg, "in_job", 6)) {
+                if(!strncmp(col + 1, "shared", 6)) {
+                    adhoc_mode = ADM_ADHOC_MODE_IN_JOB_SHARED;
+                    parsed = 1;
+                }
+                if(!strncmp(col + 1, "dedicated", 9)) {
+                    adhoc_mode = ADM_ADHOC_MODE_IN_JOB_DEDICATED;
+                    parsed = 1;
+                }
+            } else if(!strncmp(optarg, "separate", 8)) {
+                if(!strncmp(col + 1, "new", 3)) {
+                    adhoc_mode = ADM_ADHOC_MODE_SEPARATE_NEW;
+                    parsed = 1;
+                }
+                if(!strncmp(col + 1, "existing", 8)) {
+                    adhoc_mode = ADM_ADHOC_MODE_SEPARATE_EXISTING;
+                    parsed = 1;
+                }
+            }
+        }
 
-	return 0;
+        if(!parsed) {
+            return -1;
+        }
+    }
+
+    if(tag == TAG_CONTEXT_ID) {
+        strncpy(adhoc_id, optarg, ADHOCID_LEN - 1);
+        adhoc_id[ADHOCID_LEN - 1] = '\0';
+    }
+
+    return 0;
 }
 
 static int
-scord_register_job(const char *scord_proto, const char *scord_addr, const char *nodelist, uint32_t jobid)
-{
-	int rc = 0;
+scord_register_job(const char* scord_proto, const char* scord_addr,
+                   const char* nodelist, uint32_t jobid) {
+    int rc = 0;
 
-	ADM_server_t scord_server;
-	scord_server = ADM_server_create(scord_proto, scord_addr);
-	if (!scord_server) {
-		slurm_error("slurmadmcli: scord server creation failed");
-		rc = -1;
-		goto end;
-	}
+    ADM_server_t scord_server;
+    scord_server = ADM_server_create(scord_proto, scord_addr);
+    if(!scord_server) {
+        slurm_error("slurmadmcli: scord server creation failed");
+        rc = -1;
+        goto end;
+    }
 
-	/* list of job nodes */
-	hostlist_t hl = slurm_hostlist_create(nodelist);
-	if (!hl) {
-		slurm_error("slurmadmcli: slurm_hostlist creation failed");
-		rc = -1;
-		goto end;
-	}
+    /* list of job nodes */
+    hostlist_t hl = slurm_hostlist_create(nodelist);
+    if(!hl) {
+        slurm_error("slurmadmcli: slurm_hostlist creation failed");
+        rc = -1;
+        goto end;
+    }
 
-	int nnodes = slurm_hostlist_count(hl);
-	if (nnodes <= 0) {
-		slurm_error("slurmadmcli: wrong slurm_hostlist count");
-		rc = -1;
-		goto end;
-	}
+    int nnodes = slurm_hostlist_count(hl);
+    if(nnodes <= 0) {
+        slurm_error("slurmadmcli: wrong slurm_hostlist count");
+        rc = -1;
+        goto end;
+    }
 
-	ADM_node_t *nodes = reallocarray(NULL, nnodes, sizeof(ADM_node_t));
-	if (!nodes) {
-		slurm_error("slurmadmcli: out of memory");
-		rc = -1;
-		goto end;
-	}
+    ADM_node_t* nodes = reallocarray(NULL, nnodes, sizeof(ADM_node_t));
+    if(!nodes) {
+        slurm_error("slurmadmcli: out of memory");
+        rc = -1;
+        goto end;
+    }
 
-	size_t i = 0;
-	char *nodename;
-	while((nodename = slurm_hostlist_shift(hl))) {
-		nodes[i] = ADM_node_create(nodename, ADM_NODE_REGULAR);
-		if (!nodes[i]) {
-			slurm_error("slurmadmcli: scord node creation failed");
-			rc = -1;
-			goto end;
-		}
-		i++;
-	}
+    size_t i = 0;
+    char* nodename;
+    while((nodename = slurm_hostlist_shift(hl))) {
+        nodes[i] = ADM_node_create(nodename, ADM_NODE_REGULAR);
+        if(!nodes[i]) {
+            slurm_error("slurmadmcli: scord node creation failed");
+            rc = -1;
+            goto end;
+        }
+        i++;
+    }
 
-	ADM_job_resources_t job_resources;
-	job_resources = ADM_job_resources_create(nodes, nnodes);
-	if (!job_resources) {
-		slurm_error("slurmadmcli: job_resources creation failed");
-		rc = -1;
-		goto end;
-	}
+    ADM_job_resources_t job_resources;
+    job_resources = ADM_job_resources_create(nodes, nnodes);
+    if(!job_resources) {
+        slurm_error("slurmadmcli: job_resources creation failed");
+        rc = -1;
+        goto end;
+    }
 
-	/* take the ADHOC_NNODES first nodes for the adhoc */
-	ADM_adhoc_resources_t adhoc_resources;
-	adhoc_resources = ADM_adhoc_resources_create(nodes, adhoc_nnodes < nnodes ? adhoc_nnodes : nnodes);
-	if (!adhoc_resources) {
-		slurm_error("slurmadmcli: adhoc_resources creation failed");
-		rc = -1;
-		goto end;
-	}
+    /* take the ADHOC_NNODES first nodes for the adhoc */
+    ADM_adhoc_resources_t adhoc_resources;
+    adhoc_resources = ADM_adhoc_resources_create(
+            nodes, adhoc_nnodes < nnodes ? adhoc_nnodes : nnodes);
+    if(!adhoc_resources) {
+        slurm_error("slurmadmcli: adhoc_resources creation failed");
+        rc = -1;
+        goto end;
+    }
 
-	ADM_adhoc_context_t adhoc_ctx;
+    ADM_adhoc_context_t adhoc_ctx;
     adhoc_ctx = ADM_adhoc_context_create(
             NULL, adhoc_mode, ADM_ADHOC_ACCESS_RDWR, adhoc_walltime, false);
     if(!adhoc_ctx) {
         slurm_error("slurmadmcli: adhoc_context creation failed");
-		rc = -1;
-		goto end;
-	}
+        rc = -1;
+        goto end;
+    }
 
     ADM_adhoc_storage_t adhoc_storage;
     if(ADM_register_adhoc_storage(
@@ -255,22 +241,23 @@ scord_register_job(const char *scord_proto, const char *scord_addr, const char *
     }
 
     /* no inputs or outputs */
-	ADM_job_requirements_t scord_reqs;
-	scord_reqs = ADM_job_requirements_create(NULL, 0, NULL, 0, adhoc_storage);
-	if (!scord_reqs) {
-		slurm_error("slurmadmcli: scord job_requirements creation");
-		rc = -1;
-		goto end;
-	}
+    ADM_job_requirements_t scord_reqs;
+    scord_reqs = ADM_job_requirements_create(NULL, 0, NULL, 0, adhoc_storage);
+    if(!scord_reqs) {
+        slurm_error("slurmadmcli: scord job_requirements creation");
+        rc = -1;
+        goto end;
+    }
 
-	ADM_job_t scord_job;
-	if (ADM_register_job(scord_server, job_resources, scord_reqs, jobid, &scord_job) != ADM_SUCCESS) {
-		slurm_error("slurmadmcli: scord job registration failed");
-		rc = -1;
-		goto end;
-	}
+    ADM_job_t scord_job;
+    if(ADM_register_job(scord_server, job_resources, scord_reqs, jobid,
+                        &scord_job) != ADM_SUCCESS) {
+        slurm_error("slurmadmcli: scord job registration failed");
+        rc = -1;
+        goto end;
+    }
 
-    if (ADM_deploy_adhoc_storage(scord_server, adhoc_storage, NULL) !=
+    if(ADM_deploy_adhoc_storage(scord_server, adhoc_storage, NULL) !=
        ADM_SUCCESS) {
         slurm_error("slurmadmcli: adhoc storage deployment failed");
         rc = -1;
@@ -278,95 +265,104 @@ scord_register_job(const char *scord_proto, const char *scord_addr, const char *
     }
 
 end:
-	slurm_hostlist_destroy(hl);
-	ADM_adhoc_resources_destroy(adhoc_resources);
-	ADM_remove_job(scord_server, scord_job);
-	ADM_job_requirements_destroy(scord_reqs);
-	ADM_adhoc_storage_destroy(adhoc_storage);
-	ADM_server_destroy(scord_server);
-	return rc;
+    slurm_hostlist_destroy(hl);
+    ADM_adhoc_resources_destroy(adhoc_resources);
+    ADM_remove_job(scord_server, scord_job);
+    ADM_job_requirements_destroy(scord_reqs);
+    ADM_adhoc_storage_destroy(adhoc_storage);
+    ADM_server_destroy(scord_server);
+    return rc;
 }
 
 int
-slurm_spank_init(spank_t sp, int ac, char **av)
-{
-	(void)ac;
-	(void)av;
+slurm_spank_init(spank_t sp, int ac, char** av) {
+    (void) ac;
+    (void) av;
 
-	spank_err_t rc = ESPANK_SUCCESS;
+    spank_err_t rc = ESPANK_SUCCESS;
 
-	spank_context_t sctx = spank_context();
-	if (sctx == S_CTX_LOCAL || sctx == S_CTX_ALLOCATOR) {
-		/* register adm/scord options */
-		struct spank_option *opt = &spank_opts[0];
-		while(opt->name) {
-			rc = spank_option_register(sp, opt++);
-		}
-	}
+    spank_context_t sctx = spank_context();
+    if(sctx == S_CTX_LOCAL || sctx == S_CTX_ALLOCATOR) {
+        /* register adm/scord options */
+        struct spank_option* opt = &spank_opts[0];
+        while(opt->name) {
+            rc = spank_option_register(sp, opt++);
+        }
+    }
 
-	return rc == ESPANK_SUCCESS ? 0 : -1;
+    return rc == ESPANK_SUCCESS ? 0 : -1;
 }
 
 
 int
-slurm_spank_local_user_init(spank_t sp, int ac, char **av)
-{
-	(void)sp;
+slurm_spank_local_user_init(spank_t sp, int ac, char** av) {
+    (void) sp;
 
-	if (!scord_flag)
-		return 0;
+    if(!scord_flag)
+        return 0;
 
-	const char *scord_addr = SCORD_SERVER_DEFAULT;
-	const char *scord_proto = SCORD_PROTO_DEFAULT;
-	const char *scordctl_bin = SCORDCTL_PROG_DEFAULT;
+    const char* scord_addr = SCORD_SERVER_DEFAULT;
+    const char* scord_proto = SCORD_PROTO_DEFAULT;
+    const char* scordctl_bin = SCORDCTL_PROG_DEFAULT;
 
-	for (int i = 0; i < ac; i++) {
-		if (!strncmp ("scord_addr=", av[i], 11)) {
-			scord_addr = av[i] + 11;
-		} else if (!strncmp ("scord_proto=", av[i], 12)) {
-			scord_proto = av[i] + 12;
-		} else if (!strncmp ("scordctl_bin=", av[i], 13)) {
-			scordctl_bin = av[i] + 13;
-		} else {
-			slurm_error("slurmadmcli: invalid option: %s", av[i]);
-			return -1;
-		}
-	}
+    for(int i = 0; i < ac; i++) {
+        if(!strncmp("scord_addr=", av[i], 11)) {
+            scord_addr = av[i] + 11;
+        } else if(!strncmp("scord_proto=", av[i], 12)) {
+            scord_proto = av[i] + 12;
+        } else if(!strncmp("scordctl_bin=", av[i], 13)) {
+            scordctl_bin = av[i] + 13;
+        } else {
+            slurm_error("slurmadmcli: invalid option: %s", av[i]);
+            return -1;
+        }
+    }
 
-	spank_err_t rc;
+    spank_err_t rc;
 
-	/* get job id */
-	uint32_t jobid;
-	char sjobid[INT32_STR_LEN];
-	if ((rc = spank_get_item(sp, S_JOB_ID, &jobid)) != ESPANK_SUCCESS) {
-		slurm_error ("slurmadmcli: failed to get jobid: %s", spank_strerror(rc));
-		return -1;
-	}
-	if (snprintf(sjobid, INT32_STR_LEN, "%"PRIu32, jobid) < 0) {
-		slurm_error ("slurmadmcli: failed to convert jobid");
-		return -1;
-	}
+    /* get job id */
+    uint32_t jobid;
+    char sjobid[INT32_STR_LEN];
+    if((rc = spank_get_item(sp, S_JOB_ID, &jobid)) != ESPANK_SUCCESS) {
+        slurm_error("slurmadmcli: failed to get jobid: %s", spank_strerror(rc));
+        return -1;
+    }
+    if(snprintf(sjobid, INT32_STR_LEN, "%" PRIu32, jobid) < 0) {
+        slurm_error("slurmadmcli: failed to convert jobid");
+        return -1;
+    }
 
-	/* get list of nodes. /!\ at this point env SLURM_NODELIST is
-	   set, but not SLURM_JOB_NODELIST! */
-	const char *nodelist = getenv("SLURM_NODELIST");
-	if (!nodelist) {
-		slurm_error("slurmadmcli: failed to get node list");
-		return -1;
-	}
+    /* get list of nodes. /!\ at this point env SLURM_NODELIST is
+       set, but not SLURM_JOB_NODELIST! */
+    const char* nodelist = getenv("SLURM_NODELIST");
+    if(!nodelist) {
+        slurm_error("slurmadmcli: failed to get node list");
+        return -1;
+    }
 
 
-	/* launch one scord-ctl task on one node in the allocation, let Slurm decide which */
-	pid_t pid;
-	if ((pid = fork()) < 0) {
-		slurm_error("slurmadmcli: failed to start scord-ctl: %s", strerror(errno));
-		return -1;
-	} else if (pid == 0) {
-		char *argv[] = { "srun", "-N1", "-n1", "--overlap", "--cpu-bind=none", "--jobid", sjobid, (char *const)scordctl_bin, NULL };
-		execvp(argv[0], argv);
-		slurm_error("slurmadmcli: failed to srun scord-ctl: %s", strerror(errno));
-		return 0;
-	}
+    /* launch one scord-ctl task on one node in the allocation, let Slurm decide
+     * which */
+    pid_t pid;
+    if((pid = fork()) < 0) {
+        slurm_error("slurmadmcli: failed to start scord-ctl: %s",
+                    strerror(errno));
+        return -1;
+    } else if(pid == 0) {
+        char* argv[] = {"srun",
+                        "-N1",
+                        "-n1",
+                        "--overlap",
+                        "--cpu-bind=none",
+                        "--jobid",
+                        sjobid,
+                        (char* const) scordctl_bin,
+                        NULL};
+        execvp(argv[0], argv);
+        slurm_error("slurmadmcli: failed to srun scord-ctl: %s",
+                    strerror(errno));
+        return 0;
+    }
 
-	return scord_register_job(scord_proto, scord_addr, nodelist, jobid);
+    return scord_register_job(scord_proto, scord_addr, nodelist, jobid);
 }
